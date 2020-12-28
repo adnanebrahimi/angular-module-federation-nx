3b69cddf37eb6d05a2fe8778fa84c3bfcd190ce2
Adnan Ebrahimi <adnan.ebrahimi@gmail.com>
2020-12-28 18:21:20 +0330

    Finish :)

--- apps/shell/src/app/app-routing.module.ts
+++ apps/shell/src/app/app-routing.module.ts
@@ -9,45 +9,45 @@ const routes: Routes = [
     loadChildren: () =>
       import('./features/home/home.module').then((m) => m.HomeModule),
   },
-  {
-    path: 'one',
-    loadChildren: () => import('one/Module').then((m) => m.HomeOneModule),
-  },
-  {
-    path: 'two',
-    loadChildren: () => import('two/Module').then((m) => m.HomeTwoModule),
-  },
-  {
-    path: 'three',
-    loadChildren: () => import('three/Module').then((m) => m.HomeThreeModule),
-  },
   // {
   //   path: 'one',
-  //   loadChildren: () =>
-  //     loadRemoteModule({
-  //       exposedModule: './Module',
-  //       remoteName: 'one',
-  //       remoteEntry:'http://localhost:3000/remoteEntry.js'
-  //     }).then((m) => m.HomeOneModule),
+  //   loadChildren: () => import('one/Module').then((m) => m.HomeOneModule),
   // },
   // {
   //   path: 'two',
-  //   loadChildren: () =>
-  //     loadRemoteModule({
-  //       exposedModule: './Module',
-  //       remoteName: 'two',
-  //       remoteEntry:'http://localhost:3500/remoteEntry.js'
-  //     }).then((m) => m.HomeTwoModule),
+  //   loadChildren: () => import('two/Module').then((m) => m.HomeTwoModule),
   // },
   // {
   //   path: 'three',
-  //   loadChildren: () =>
-  //     loadRemoteModule({
-  //       exposedModule: './Module',
-  //       remoteName: 'three',
-  //       remoteEntry:'http://localhost:5500/remoteEntry.js'
-  //     }).then((m) => m.HomeThreeModule),
+  //   loadChildren: () => import('three/Module').then((m) => m.HomeThreeModule),
   // },
+  {
+    path: 'one',
+    loadChildren: () =>
+      loadRemoteModule({
+        exposedModule: './Module',
+        remoteName: 'one',
+        remoteEntry:'http://localhost:3000/remoteEntry.js'
+      }).then((m) => m.HomeOneModule),
+  },
+  {
+    path: 'two',
+    loadChildren: () =>
+      loadRemoteModule({
+        exposedModule: './Module',
+        remoteName: 'two',
+        remoteEntry:'http://localhost:3500/remoteEntry.js'
+      }).then((m) => m.HomeTwoModule),
+  },
+  {
+    path: 'three',
+    loadChildren: () =>
+      loadRemoteModule({
+        exposedModule: './Module',
+        remoteName: 'three',
+        remoteEntry:'http://localhost:5500/remoteEntry.js'
+      }).then((m) => m.HomeThreeModule),
+  },
 ];
 
 @NgModule({
